928cba332e2923698e66a2e2a552d8fbfa28c282
Karthik Santhosh <karthik.saihasoft@gmail.com>
2025-08-05 15:32:57 +0530

    updates

---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => (
+  <div className="spinner">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default Loader;
